22a50749747794d99337335a1af096600f17d56d
Santiago <santiagosaav.99@gmail.com>
2020-11-22 18:08:43 -0300

    Se ha cambiado la función fileCopy por estamparDocumento
    estamparDocumento permite insertar una imagen o estampado al final de un documento

--- reader.py
+++ reader.py
@@ -1,24 +1,35 @@
 from shutil import copyfile #Función para copiar archivos
-
+from docx import Document #Pip install python-docx
+from docx.shared import Inches
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 #-----------------------------------------
 #         Lector de USB
 #-----------------------------------------
 
 def usbReader():
-    data = None
+    clave_privada = None
     try:
         with open('E:/token.txt', 'r') as file:
-            data = file.read()
-            return data
+            clave_privada = file.read()
+            return clave_privada
     except:
         print("Error al obtener el Token del USB, intente nuevamente.")
         return None
         
 #-----------------------------------------
-#         Copia de Archivos
+#         Firma el documento
 #-----------------------------------------
-def fileCopy(directory): #Recibe el directorio del archivo a copiar
-    path = r'./files/test.txt'
-    copyfile(directory, path)
 
-fileCopy('D:/aa.txt')
+def estamparDocumento(directorio_documento, nombre_archivo, estampado):
+    try:
+        path = r'./files/'+nombre_archivo+'.docx'
+        documento = Document(directorio_documento)
+        firma = documento.add_paragraph() #Crea un párrafo
+        firma.alignment=  WD_PARAGRAPH_ALIGNMENT.RIGHT #Y lo alinea a la derecha
+        ejecutar = firma.add_run("") 
+        ejecutar.add_picture(estampado, width=Inches(1.75)) #Y se añade la imagen al párrafo
+        documento.save(path)
+    except:
+        print("Error al firmar el documento.")
+
+
