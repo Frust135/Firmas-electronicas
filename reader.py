138fee45df0091ae2360495a62436f3913459262
Ines <ines.apablaza@alumnos.ucentral.cl>
2020-11-22 18:40:43 -0300

    Implementación de lectura (docx) y función hash

--- reader.py
+++ reader.py
@@ -1,4 +1,6 @@
 from shutil import copyfile #Función para copiar archivos
+import hashlib
+import docx
 
 #########################################
 #         Lector de USB
@@ -7,7 +9,7 @@ from shutil import copyfile #Función para copiar archivos
 def usbReader():
     data = None
     try:
-        with open('E:/token.txt', 'r') as file:
+        with open('D:/token.txt', 'r') as file:
             data = file.read()
             return data
     except:
@@ -20,3 +22,27 @@ def usbReader():
 def fileCopy(directory): #Recibe el directorio del archivo a copiar
     path = r'./files/test.txt'
     copyfile(directory, path)
+
+#############################################
+#         Obtener texto de todo el documento
+#############################################
+
+def getText(filename):
+    doc = docx.Document(filename)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return fullText
+
+#print(getText("documento-firmado.docx"))
+
+#############################################
+#         Obtener el hash del documento
+#############################################
+
+hash_md5 = hashlib.md5() 
+documento = getText("documento-firmado.docx")
+texto = documento[1]
+hash_md5.update(texto.encode())
+print (hash_md5.hexdigest())
+
