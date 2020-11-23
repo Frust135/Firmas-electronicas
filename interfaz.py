d4858cce4d600a0a2d09e2efd41f8461ca581a64
Ines <ines.apablaza@alumnos.ucentral.cl>
2020-11-23 19:20:28 -0300

    Se ha completado la interfaz del box 1

--- interfaz.py
+++ interfaz.py
@@ -28,33 +28,37 @@ f.place(x=40, y=133)
 nombre_archivo = Label(text = "Nombre del archivo", font=("Cambria",15),fg="#1EEB74" , bg="#1E136E", width="25", height="1")
 nombre_archivo.place(x=25, y=150)
 
-ingreso_nombre_archivo = Entry(width = "42")
+ingreso_nombre_archivo = Entry(width = "42",bg="#B6ADE4")
 ingreso_nombre_archivo.place(x=40, y=180)
 
 seleccion_archivo = Label(text = "Seleccion",font=("Cambria",15),fg="#1EEB74" , bg="#1E136E", width="25", height="1")
 seleccion_archivo.place(x=25, y= 220)
 
-boton_seleccion_archivo =  Button(myWindow, text = "Seleccione el archivo")
-boton_seleccion_archivo.place(x=105, y=250)
+boton_seleccion_archivo =  Button(myWindow, text = "Seleccione el archivo" ,width="35", bg="#B6ADE4")
+boton_seleccion_archivo.place(x=40, y=250)
 
 clave_publica = Label(text = "Ingrese clave publica",font=("Cambria",15),fg="#1EEB74" , bg="#1E136E", width="25", height="1")
 clave_publica.place(x=25, y=295)
 
-ingreso_clave_publica = Entry(width = "42")
+ingreso_clave_publica = Entry(width = "42", bg="#B6ADE4")
 ingreso_clave_publica.place(x=40, y=325)
 
+insertar_token = Label(text = "Inserte su token",font=("Cambria",15),fg="#1EEB74" , bg="#1E136E", width="25", height="1")
+insertar_token.place(x=25, y=370)
 
+boton_insercion_token = Button(myWindow, text = "Firmar", width="35", bg="#B6ADE4")
+boton_insercion_token.place(x=40, y=400)
 #-------------------------------------------------------------------
 #      Box_2: Obtener documento
 #-------------------------------------------------------------------
-box2=Label(bg="#1E136E", width="40", height="21")
+box2=Label(bg="#1E136E", width="40", height="25")
 box2.place(x=350, y=100)
 
 #-------------------------------------------------------------------
 #      Box_3: Revisar firma
 #-------------------------------------------------------------------
 
-box3=Label(bg="#1E136E", width="40", height="21")
+box3=Label(bg="#1E136E", width="40", height="25")
 box3.place(x=675, y=100)
 
 myWindow.mainloop()
